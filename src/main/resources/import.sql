3af762b42b60a67a5938fe7141d7445800ca9c2f
Nuh <numoh.55@gmail.com>
2023-12-09 15:06:16 +0100

    Inserting data into db
    Inserting data into the tables:
    1. user
    2. patient
    3. doctor
    4. staff

---
+++ src/main/resources/import.sql
@@ -0,0 +1,48 @@
+-- Inserting data into the user table
+
+-- PATIENTS
+INSERT INTO `user` (address, email, firstname, lastname, password, phone, role) VALUES ('Edingekroken 13', 'numoh.55@gmail.com', 'Nuh', 'Jama Mohamud', '$2a$12$/C8fviFbdCDQhc9l3R7gl.I5hrWw6czE5JV3pp29WkiiScy4WQc6u', '0765641298', 'PATIENT'); -- PATIENT1
+INSERT INTO `user` (address, email, firstname, lastname, password, phone, role) VALUES ('Patient Address 2', 'patient2@example.com', 'Patient', 'Two', '$2a$12$V8TG69Luekm4HG3rANA6bervv8ckBFKgMpbBD1dMk.JCEHGl/9Osa', '9876543210', 'PATIENT'); -- PATIENT2
+INSERT INTO `user` (address, email, firstname, lastname, password, phone, role) VALUES ('Patient Address 3', 'patient3@example.com', 'Patient', 'Three', '$2a$12$j.f.Q74nBH9w5EUm.t/Zd.fBrVLEtyO00FWQyV2LgN6bstWMA2Wmm', '1234567890', 'PATIENT'); -- PATIENT3
+INSERT INTO `user` (address, email, firstname, lastname, password, phone, role) VALUES ('Patient Address 4', 'patient4@example.com', 'Patient', 'Four', '$2a$12$azdEtYdGELA9Tz/GxZI6wOj4T1GiWNpF4iKqm1xZTbNrSGUq9TmeW', '2345678901', 'PATIENT'); -- PATIENT4
+INSERT INTO `user` (address, email, firstname, lastname, password, phone, role) VALUES ('Patient Address 5', 'patient5@example.com', 'Patient', 'Five', '$2a$12$dL7hTjOtao8I4hYKHCZ/K.tFSfKocPb6gKRXk8zMdYWWY.bQyTXBu', '3456789012', 'PATIENT'); -- PATIENT5
+
+-- DOCTOR
+INSERT INTO `user` (address, email, firstname, lastname, password, phone, role) VALUES ('Doctor Address 1', 'doctor1@example.com', 'Doctor', 'One', '$2a$12$2k8skPFOh0o1nzrh9O9EEOZ4Z9ihHIuCm5ZjqERMYGLfIUGKcWOpS', '1234567890', 'DOCTOR'); -- DOCTOR1
+INSERT INTO `user` (address, email, firstname, lastname, password, phone, role) VALUES ('Doctor Address 2', 'doctor2@example.com', 'Doctor', 'Two', '$2a$12$ycph9Lb40jxLdbtU/etNRuZLEbKOdlv1tHyYM1A1gjn2gKamcDmWy', '2345678901', 'DOCTOR'); -- DOCTOR2
+INSERT INTO `user` (address, email, firstname, lastname, password, phone, role) VALUES ('Doctor Address 3', 'doctor3@example.com', 'Doctor', 'Three', '$2a$12$PW2drDWKY1KtLRS4BKzRweN/oiuRJ7rnvKjqVsLPULpIprmMabpxe', '3456789012', 'DOCTOR'); -- DOCTOR3
+INSERT INTO `user` (address, email, firstname, lastname, password, phone, role) VALUES ('Doctor Address 4', 'doctor4@example.com', 'Doctor', 'Four', '$2a$12$SV0gY9SG/qTgT47MlkA97.PDEskyufbZ.IAGGAKOtJpjJf5ab60.e', '4567890123', 'DOCTOR'); -- DOCTOR4
+INSERT INTO `user` (address, email, firstname, lastname, password, phone, role) VALUES ('Doctor Address 5', 'doctor5@example.com', 'Doctor', 'Five', '$2a$12$AxGDdFVkSR4uYdxjvWdqzuyqMBwBBiiu2LmK1cpklR6BLMf35O0su', '5678901234', 'DOCTOR'); -- DOCTOR5
+INSERT INTO `user` (address, email, firstname, lastname, password, phone, role) VALUES ('Doctor Address 6', 'doctor6@example.com', 'Doctor', 'Six', '$2a$12$xczbZHUsmQj4cOPm3u8XaeYfFm38bfQDdE3dxNJ5FhvcJiFVa0Qz6', '6789012345', 'DOCTOR'); -- DOCTOR6
+
+-- STAFF
+INSERT INTO `user` (address, email, firstname, lastname, password, phone, role) VALUES ('Staff Address 1', 'staff1@example.com', 'Staff', 'One', '$2a$12$1Ot654VVJwuhiRd4P4TpTOccoYN.FcX8GGEQnyusJQE9QZeSCg446', '9876543210', 'STAFF'); -- STAFF1
+INSERT INTO `user` (address, email, firstname, lastname, password, phone, role) VALUES ('Staff Address 2', 'staff2@example.com', 'Staff', 'Two', '$2a$12$cwpXiCjUYrX4GeFszZBTceglgMFyY5WTF1q2HjEkaBm3uig.003tG', '1234567890', 'STAFF'); -- STAFF2
+INSERT INTO `user` (address, email, firstname, lastname, password, phone, role) VALUES ('Staff Address 3', 'staff3@example.com', 'Staff', 'Three', '$2a$12$8YPBJkIJDu3yjIUp4G/tq.kC3qvlLmK62BaSZKEAK8iXrnckcMqaa', '2345678901', 'STAFF'); -- STAFF3
+INSERT INTO `user` (address, email, firstname, lastname, password, phone, role) VALUES ('Staff Address 4', 'staff4@example.com', 'Staff', 'Four', '$2a$12$ujuwVKuAoHUGX0Yr7qCbm.OsuCSZBRZRG3FcT7pxUAwGQ.ePrAk26', '3456789012', 'STAFF'); -- STAFF4
+INSERT INTO `user` (address, email, firstname, lastname, password, phone, role) VALUES ('Staff Address 5', 'staff5@example.com', 'Staff', 'Five', '$2a$12$YLQLkp9z7yry1K1ujvH59OL0masOkw/Xu7OAATas7eGiJRBGye48i', '4567890123', 'STAFF'); -- STAFF5
+INSERT INTO `user` (address, email, firstname, lastname, password, phone, role) VALUES ('Staff Address 6', 'staff6@example.com', 'Staff', 'Six', '$2a$12$6jo/wnH3WHgAQQpa0kmbZ.mSY1MIbN6m58Ahel7HidR1VjzOtZkRe', '5678901234', 'STAFF'); -- STAFF6
+
+
+# Inserting data into the patient table
+INSERT INTO patient (id) VALUES (1);
+INSERT INTO patient (id) VALUES (2);
+INSERT INTO patient (id) VALUES (3);
+INSERT INTO patient (id) VALUES (4);
+INSERT INTO patient (id) VALUES (5);
+
+# Inserting data into the doctor table
+INSERT INTO doctor (id) VALUES (6);
+INSERT INTO doctor (id) VALUES (7);
+INSERT INTO doctor (id) VALUES (8);
+INSERT INTO doctor (id) VALUES (9);
+INSERT INTO doctor (id) VALUES (10);
+INSERT INTO doctor (id) VALUES (11);
+
+# Inserting data into the staff table
+INSERT INTO staff (id) VALUES (12);
+INSERT INTO staff (id) VALUES (13);
+INSERT INTO staff (id) VALUES (14);
+INSERT INTO staff (id) VALUES (15);
+INSERT INTO staff (id) VALUES (16);
+INSERT INTO staff (id) VALUES (17);

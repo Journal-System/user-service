e308adbd834ef756cd4fa896c168d7b8c969caf7
Nuh <numoh.55@gmail.com>
2023-12-20 21:10:33 +0200

    little fix

--- src/test/java/kth/numi/userservice/controller/AuthenticationControllerTest.java
+++ src/test/java/kth/numi/userservice/controller/AuthenticationControllerTest.java
@@ -14,7 +14,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
